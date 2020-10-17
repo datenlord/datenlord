2aad8d54be3081a9fb757dfb7d2bbe7137c8a415
Pu Wang <nicolas.weeks@gmail.com>
2020-10-17 10:12:33 +0000

    fix k8s e2e

--- umount-in-container.sh
+++ umount-in-container.sh
@@ -2,6 +2,7 @@
 
 set -xv # enable debug
 set -e # exit on error
+set -u # unset var as error
 
 if [ $# -le 0 ]
 then
@@ -11,20 +12,27 @@ fi
 
 FUSECTL_DIR=/sys/fs/fuse/connections
 
-FUSE_CTL_MOUNTED=$(cat /proc/self/mountinfo | grep $FUSECTL_DIR | awk '{print $5}')
-if [ $FUSE_CTL_MOUNTED ]
+FUSE_DIR=$1
+MOUNTED=$(cat /proc/self/mountinfo | grep fuse | grep $FUSE_DIR | awk '{print $5}')
+if [ -z $MOUNTED ]
 then
-    echo "FUSECTL IS MOUNTED"
+    echo "$FUSE_DIR UN-MOUNTED"
 else
-    echo "MOUNT FUSECTL"
-    mount -t fusectl fusectl $FUSECTL_DIR
+    umount -f $FUSE_DIR || echo "UMOUNT FAILED"
 fi
 
-FUSE_DIR=$1
 FUSE_MINOR=$(cat /proc/self/mountinfo | grep fuse | grep $FUSE_DIR | awk '{print $3}' | cut -d ':' -f 2)
 if [ -z $FUSE_MINOR ]
 then
     echo "$FUSE_DIR IS NOT MOUNTED"
 else
+    FUSE_CTL_MOUNTED=$(cat /proc/self/mountinfo | grep $FUSECTL_DIR | awk '{print $5}')
+    if [ $FUSE_CTL_MOUNTED ]
+    then
+        echo "FUSECTL IS MOUNTED"
+    else
+        echo "MOUNT FUSECTL"
+        mount -t fusectl fusectl $FUSECTL_DIR
+    fi
     echo "UMOUNT FUSE DIR=$FUSE_DIR MINOR=$FUSE_MINOR" | tee /sys/fs/fuse/connections/$FUSE_MINOR/abort
 fi
