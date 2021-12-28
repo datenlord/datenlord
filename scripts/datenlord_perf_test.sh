0ca5285e83710365493bf0d6db250c0346500195
Zheng Pan <zhengpan@google.com>
2021-12-27 21:27:01 -0800

    Enhance datenlord fio perf test.
    Add more test cases and generate result graphs.

--- scripts/datenlord_perf_test.sh
+++ scripts/datenlord_perf_test.sh
@@ -7,19 +7,25 @@ set -u # unset var as error
 readonly PERF_CONFIG="scripts/datenlord-perf.yaml"
 readonly NAMESPACE="csi-datenlord"
 readonly ASYNC_FUSE_APP="datenlord-async-fuse"
-readonly FIO_SCRIPT="fio_perf.fio"
 readonly TEST_DIR="/var/opt/datenlord-data"
-readonly TEST_LOG="/tmp/perf_log.txt"
+readonly OUTPUT_DIR="/tmp/output"
+readonly PERF_SCRIPT="fio_perf_test.sh"
 
+# Deploy datenlord for perf test
 kubectl apply -f ${PERF_CONFIG}
-kubectl wait --for=condition=Ready pod -l app=${ASYNC_FUSE_APP} -n ${NAMESPACE} --timeout=60s
-# Sleep 10s to make sure the cluster is stable
-sleep 10
+# Sleep 30s to make sure the cluster is stable
+sleep 30
+kubectl wait --for=condition=Ready pod -l app=${ASYNC_FUSE_APP} -n ${NAMESPACE} --timeout=120s
 
 
 FIRST_NODE=$(kubectl get pods -A | grep "datenlord-async" | awk 'NR==1{print $2}')
 
 kubectl exec ${FIRST_NODE} -n ${NAMESPACE} -- apt-get update
-kubectl exec ${FIRST_NODE} -n ${NAMESPACE} -- apt-get install -y fio
-kubectl cp scripts/${FIO_SCRIPT} ${FIRST_NODE}:/tmp/${FIO_SCRIPT} -n ${NAMESPACE}
-kubectl exec ${FIRST_NODE} -n ${NAMESPACE} -- env TEST_DIR=${TEST_DIR} fio /tmp/${FIO_SCRIPT} > ${TEST_LOG}
+kubectl exec ${FIRST_NODE} -n ${NAMESPACE} -- apt-get install -y fio python3-pip
+kubectl exec ${FIRST_NODE} -n ${NAMESPACE} -- pip3 install matplotlib numpy fio-plot
+kubectl cp scripts/${PERF_SCRIPT} ${FIRST_NODE}:/tmp -n ${NAMESPACE}
+kubectl exec ${FIRST_NODE} -n ${NAMESPACE} -- sh /tmp/${PERF_SCRIPT} ${TEST_DIR}
+
+rm -rf ${OUTPUT_DIR}
+mkdir ${OUTPUT_DIR}
+kubectl cp ${FIRST_NODE}:${OUTPUT_DIR} -n ${NAMESPACE} ${OUTPUT_DIR}
---
+++ scripts/fio_perf_test.sh
@@ -0,0 +1,38 @@
+#/bin/sh
+
+set -xv # enable debug
+set -e # exit on error
+set -u # unset var as error
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 TEST_DIR" >&2
+    exit 1
+fi
+
+readonly TEST_DIR="$1"
+readonly OUTPUT_DIR="/tmp/output"
+readonly TEST_DIR_BASE="$(basename ${TEST_DIR})"
+readonly OUTPUT_SUBDIR="${OUTPUT_DIR}/${TEST_DIR_BASE}"
+
+# Test paramaters
+readonly BLOCK_SIZES="4k 8k 16k 32k 64k 128k 256k 512k"
+readonly BLOCK_SIZES_DIRS="$(
+    block_size_dirs=""
+    for block_size in ${BLOCK_SIZES}; do
+        block_size_dirs="${block_size_dirs} ${OUTPUT_SUBDIR}/${block_size}"
+    done
+    echo ${block_size_dirs}
+)"
+readonly THREAD_NUMS="1 2 4 8 16 32"
+
+rm -rf ${OUTPUT_DIR}
+mkdir ${OUTPUT_DIR}
+# Test different block sizes
+bench-fio --target ${TEST_DIR} --type directory --size 10M --output ${OUTPUT_DIR} --iodepth 1 --block-size ${BLOCK_SIZES} --numjobs 1 --engine sync --direct 1
+fio-plot -i ${BLOCK_SIZES_DIRS} -T "Different Block Size Write" -C --rw randwrite --xlabel-parent 0 -o ${OUTPUT_DIR}/block_size_write.png
+fio-plot -i ${BLOCK_SIZES_DIRS} -T "Different Block Size Read" -C --rw randread --xlabel-parent 0 -o ${OUTPUT_DIR}/block_size_read.png
+
+# Test different thread numbers
+bench-fio --target ${TEST_DIR} --type directory --size 5M --output ${OUTPUT_DIR} --iodepth 1 --block-size 4k --numjobs ${THREAD_NUMS} --engine sync --direct 1
+fio-plot -i ${OUTPUT_SUBDIR}/4k/  -T "Different Thread Num Write" -N --rw randwrite -o ${OUTPUT_DIR}/4k_multithread_write.png
+fio-plot -i ${OUTPUT_SUBDIR}/4k/  -T "Different Thread Num Read" -N --rw randread -o ${OUTPUT_DIR}/4k_multithread_read.png
