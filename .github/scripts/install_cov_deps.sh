b85645c7f07a9b67f8f74a80be0a0e8baf5ad334
xiaguan <751080330@qq.com>
2023-06-25 00:33:49 +0800

    CI/Cov update

--- .github/scripts/install_cov_deps.sh
+++ .github/scripts/install_cov_deps.sh
@@ -6,7 +6,5 @@ wget https://github.com/protocolbuffers/protobuf/releases/download/v21.10/protoc
 unzip protoc-21.10-linux-x86_64.zip -d .local
 mv "$(pwd)/.local/bin/protoc" /bin/
 
-# install grcov
-wget https://github.com/mozilla/grcov/releases/download/v0.8.13/grcov-x86_64-unknown-linux-gnu.tar.bz2
-tar -xjvf grcov-x86_64-unknown-linux-gnu.tar.bz2
-mv grcov /bin/
+# install cargo-llvm-cov
+cargo install cargo-llvm-cov
