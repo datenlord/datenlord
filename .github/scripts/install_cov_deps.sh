95707d1f613c210f02c06bfe5aca6455bff68904
xiaguan <751080330@qq.com>
2023-06-25 12:55:34 +0800

    CI/Cov update

--- .github/scripts/install_cov_deps.sh
+++ .github/scripts/install_cov_deps.sh
@@ -1,12 +1,7 @@
 #/bin/bash
-apt-get install -y cmake g++
+apt install -y cmake g++ libprotobuf-dev protobuf-compiler
 
 # install protoc
 wget https://github.com/protocolbuffers/protobuf/releases/download/v21.10/protoc-21.10-linux-x86_64.zip
 unzip protoc-21.10-linux-x86_64.zip -d .local
 mv "$(pwd)/.local/bin/protoc" /bin/
-
-# install grcov
-wget https://github.com/mozilla/grcov/releases/download/v0.8.13/grcov-x86_64-unknown-linux-gnu.tar.bz2
-tar -xjvf grcov-x86_64-unknown-linux-gnu.tar.bz2
-mv grcov /bin/
