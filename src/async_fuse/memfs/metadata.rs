e72bfcb997589cf0e27099e97782dbd525b117b2
Shiroan <shiroan@qq.com>
2024-01-05 18:28:09 +0800

    refactor: handle operations with `StorageManager`

--- src/async_fuse/memfs/metadata.rs
+++ src/async_fuse/memfs/metadata.rs
@@ -5,7 +5,7 @@ use std::time::Duration;
 use async_trait::async_trait;
 use datenlord::config::StorageConfig;
 
-use super::cache::{IoMemBlock, Storage, StorageManager};
+use super::cache::{Block, Storage, StorageManager};
 use super::dist::server::CacheServer;
 use super::kv_engine::KVEngineType;
 use super::node::Node;
@@ -121,7 +121,7 @@ pub trait MetaData {
         fh: u64,
         offset: i64,
         size: u32,
-    ) -> DatenLordResult<Vec<IoMemBlock>>;
+    ) -> DatenLordResult<Vec<Block>>;
 
     /// Helper function to flush node by ino
     async fn flush(&self, ino: u64, fh: u64) -> DatenLordResult<()>;
