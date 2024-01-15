4f80589dd2aa4d06dd3200c5476d56ae25c11ea3
Shiroan <shiroan@qq.com>
2024-01-15 17:02:49 +0800

    feat: add pessimistic tests

--- src/async_fuse/memfs/cache/backend.rs
+++ src/async_fuse/memfs/cache/backend.rs
@@ -175,7 +175,9 @@ impl Storage for Backend {
         self.operator
             .remove_all(&get_file_path(ino))
             .await
-            .unwrap_or_else(|e| panic!("Failed to remove a file of ino={ino}: {e}"));
+            .unwrap_or_else(|e| {
+                panic!("Failed to remove a file from the backend of ino={ino}: {e}")
+            });
     }
 
     async fn invalidate(&self, _: INum) {
@@ -219,7 +221,7 @@ impl Storage for Backend {
                 match self.operator.read(&path).await {
                     Ok(mut dest) => {
                         dest.truncate(fill_start);
-                        self.operator.write(&path, dest).await.unwrap_or_else(|e| panic!("Failed to store a block to backend where ino={ino} and block={truncate_block_id}: {e}"));
+                        self.operator.write(&path, dest).await.unwrap_or_else(|e| panic!("Failed to store a block to backend for truncate where ino={ino} and block={truncate_block_id}: {e}"));
                     }
                     Err(e) => {
                         // It's OK that the block is not found for truncate.
@@ -479,5 +481,123 @@ mod tests {
         backend.invalidate(0).await;
 
         fs::remove_dir_all(backend_root).await.unwrap();
+    }
+
+    mod pessimistic {
+        use std::fs::Permissions;
+        use std::path::Path;
+
+        use smol::fs::unix::PermissionsExt;
+        use tokio::fs;
+
+        use super::{prepare_backend, BACKEND_ROOT, BLOCK_CONTENT, BLOCK_SIZE_IN_BYTES};
+        use crate::async_fuse::memfs::cache::{Block, Storage};
+
+        async fn cleanup(backend_root: &str) {
+            if fs::try_exists(backend_root).await.unwrap() {
+                std::process::Command::new("chmod")
+                    .args(["-R", "755", backend_root])
+                    .output()
+                    .unwrap();
+                fs::remove_dir_all(backend_root).await.unwrap();
+            }
+        }
+
+        #[tokio::test]
+        #[should_panic(expected = "backend")]
+        async fn test_failed_load() {
+            let backend_root = format!("{BACKEND_ROOT}/failed_load");
+            cleanup(&backend_root).await;
+            fs::create_dir_all(&backend_root).await.unwrap();
+            let backend = prepare_backend(&backend_root);
+
+            let backend_root = Path::new(&backend_root);
+            let file_0_path = backend_root.join("0");
+            fs::create_dir_all(&file_0_path).await.unwrap();
+            fs::write(file_0_path.join("0.block"), BLOCK_CONTENT)
+                .await
+                .unwrap();
+
+            // permission: -w-------
+            let permissions = Permissions::from_mode(0o200);
+            fs::set_permissions(file_0_path.join("0.block"), permissions)
+                .await
+                .unwrap();
+
+            // Permission Denied
+            let _: Option<Block> = backend.load(0, 0).await;
+        }
+
+        #[tokio::test]
+        #[should_panic(expected = "backend")]
+        async fn test_failed_store() {
+            let backend_root = format!("{BACKEND_ROOT}/failed_store");
+            cleanup(&backend_root).await;
+            fs::create_dir_all(&backend_root).await.unwrap();
+            let backend = prepare_backend(&backend_root);
+
+            let backend_root = Path::new(&backend_root);
+
+            // permission: r-xr-xr-x
+            let permissions = Permissions::from_mode(0o555);
+            fs::set_permissions(&backend_root, permissions)
+                .await
+                .unwrap();
+
+            // Permission Denied
+            backend
+                .store(0, 0, Block::new_zeroed(BLOCK_SIZE_IN_BYTES))
+                .await;
+        }
+
+        #[tokio::test]
+        #[should_panic(expected = "backend")]
+        async fn test_failed_remove() {
+            let backend_root = format!("{BACKEND_ROOT}/failed_remove");
+            cleanup(&backend_root).await;
+            fs::create_dir_all(&backend_root).await.unwrap();
+            let backend = prepare_backend(&backend_root);
+
+            backend
+                .store(0, 0, Block::new_zeroed(BLOCK_SIZE_IN_BYTES))
+                .await;
+
+            let backend_root = Path::new(&backend_root);
+
+            // permission: r-xr-xr-x
+            let permissions = Permissions::from_mode(0o555);
+            fs::set_permissions(&backend_root, permissions)
+                .await
+                .unwrap();
+
+            // Permission Denied
+            backend.remove(0).await;
+        }
+
+        #[tokio::test]
+        #[should_panic(expected = "truncate")]
+        async fn test_failed_truncate() {
+            let backend_root = format!("{BACKEND_ROOT}/failed_truncate");
+            cleanup(&backend_root).await;
+            fs::create_dir_all(&backend_root).await.unwrap();
+            let backend = prepare_backend(&backend_root);
+
+            for block_id in 0..8 {
+                backend
+                    .store(0, block_id, Block::new_zeroed(BLOCK_SIZE_IN_BYTES))
+                    .await;
+            }
+
+            let backend_root = Path::new(&backend_root);
+
+            // permission: r-xr-xr-x
+            let permissions = Permissions::from_mode(0o555);
+            fs::set_permissions(backend_root.join("0"), permissions)
+                .await
+                .unwrap();
+
+            // Permission Denied
+            backend.truncate(0, 8, 4, 4).await;
+        }
     }
 }
