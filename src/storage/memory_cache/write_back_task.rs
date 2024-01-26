48460abb7e90992aa8e9c9cb23c0748e236e49fa
Shiroan <shiroan@qq.com>
2024-01-26 15:49:47 +0800

    refactor: make `WriteBackTask` as a struct

--- src/storage/memory_cache/write_back_task.rs
+++ src/storage/memory_cache/write_back_task.rs
@@ -2,7 +2,7 @@
 
 use std::collections::HashMap;
 use std::mem;
-use std::sync::{Arc, Weak};
+use std::sync::Arc;
 use std::time::Duration;
 
 use clippy_utilities::OverflowArithmetic;
@@ -40,209 +40,202 @@ pub enum Command {
     Cancel(INum),
 }
 
-/// Calculate a new period for write back task's interval.
-fn calculate_period(
-    pending_length: usize,
-    command_queue_limit: usize,
-    basic_period: Duration,
-) -> Duration {
-    if pending_length > command_queue_limit.overflow_div(4).overflow_mul(3) {
-        // If the number of pending blocks is greater than 75% of `command_queue_limit`,
-        // the new period should be changed to `1/4`.
-        basic_period / 4
-    } else if pending_length > command_queue_limit.overflow_div(2) {
-        // If the number of pending blocks is greater than 50% of `command_queue_limit`,
-        // the new period should be changed to `1/2`.
-        basic_period / 2
-    } else {
-        // Otherwise, restore the basic period.
-        basic_period
-    }
-}
-
-/// Flush a pending block to the backend
-async fn flush_a_block<P, S>(
-    lru_queue: &mut LinkedHashSet<BlockCoordinate>,
-    pending_blocks: &mut HashMap<INum, HashMap<BlockId, (Block, StorageResultSender)>>,
-    cache: Arc<MemoryCache<P, S>>,
-) where
-    P: EvictPolicy<BlockCoordinate> + Send + Sync,
-    S: Storage + Send + Sync,
-{
-    if let Some(BlockCoordinate(ino, block_id)) = lru_queue.pop_front() {
-        if let Some((block, sender)) = pending_blocks
-            .get_mut(&ino)
-            .and_then(|file_level_pending_blocks| file_level_pending_blocks.remove(&block_id))
-        {
-            let res = cache.backend().store(ino, block_id, block).await;
-            sender.send(res).unwrap_or_else(|res| {
-                error!("The receiver is closed unexpectedly, with storage result: {res:?}");
-            });
-        }
-    }
-}
-
-/// Flush a file, write blocks of it to backend immediately.
-async fn flush_file<P, S>(
-    lru_queue: &mut LinkedHashSet<BlockCoordinate>,
-    pending_blocks: &mut HashMap<INum, HashMap<BlockId, (Block, StorageResultSender)>>,
-    cache: Arc<MemoryCache<P, S>>,
-    ino: INum,
-) where
-    P: EvictPolicy<BlockCoordinate> + Send + Sync,
-    S: Storage + Send + Sync,
-{
-    let file_level_pending_blocks = pending_blocks.remove(&ino);
-    lru_queue.retain_with_order(|&BlockCoordinate(i, _)| i != ino);
-
-    if let Some(file_level_pending_blocks) = file_level_pending_blocks {
-        for (block_id, (block, sender)) in file_level_pending_blocks {
-            let res = cache.backend().store(ino, block_id, block).await;
-            sender.send(res).unwrap_or_else(|res| {
-                error!("The receiver is closed unexpectedly, with storage result: {res:?}");
-            });
-        }
-    }
-}
-
-/// Flush all files, write blocks of them to backend immediately.
-async fn flush_all<P, S>(
-    lru_queue: &mut LinkedHashSet<BlockCoordinate>,
-    pending_blocks: &mut HashMap<INum, HashMap<BlockId, (Block, StorageResultSender)>>,
-    cache: Arc<MemoryCache<P, S>>,
-    sender: oneshot::Sender<()>,
-) where
-    P: EvictPolicy<BlockCoordinate> + Send + Sync,
-    S: Storage + Send + Sync,
-{
-    lru_queue.clear();
-
-    let blocks = mem::take(pending_blocks);
-
-    for (ino, file_level_pending_blocks) in blocks {
-        for (block_id, (block, block_sender)) in file_level_pending_blocks {
-            let res = cache.backend().store(ino, block_id, block).await;
-            block_sender.send(res).unwrap_or_else(|res| {
-                error!("The receiver is closed unexpectedly, with storage result: {res:?}");
-            });
-        }
-    }
-
-    sender
-        .send(())
-        .unwrap_or_else(|()| warn!("The receiver of pending block is closed unexpectedly."));
-}
-
-/// Evict blocks from the cache, if it hits the soft limit.
-async fn do_evict<P, S>(limit: SoftLimit, cache: Arc<MemoryCache<P, S>>)
-where
-    P: EvictPolicy<BlockCoordinate> + Send + Sync,
-    S: Storage + Send + Sync,
-{
-    // `a` is a numerator while `b` is a denominator, which are to represent the
-    // soft limit. If `size > capacity * a / b`, the soft limit is considered to
-    // be hit. We use the equivalent inequation `size * b / a > capacity` to
-    // avoid overflowing.
-    let SoftLimit(a, b) = limit;
-    let b = b.get();
-
-    loop {
-        let capacity = cache.policy().capacity();
-        let size = cache.policy().size();
-
-        // Soft limitation is hit, do evict.
-        if size.overflow_mul(b).overflow_div(a) > capacity {
-            if let Err(e) = cache.evict().await {
-                error!("Failed to evict a block: {e}");
-            }
-        } else {
-            break;
-        }
-    }
-}
-
-/// A function to run a write back task.
-///
-/// This task will smoothly write blocks back to the backend
-/// when the cache runs in write-back policy.
-///
-/// And this task will evict blocks to the backend if the number of blocks hits
-/// the soft limit, whether the cache runs in write-back policy or not.
-pub(super) async fn run_write_back_task<P, S>(
+/// The write back task running in the background.
+pub(super) struct WriteBackTask<P, S> {
+    /// The weak pointer of `MemoryCache`.
+    storage: Arc<MemoryCache<P, S>>,
+    /// The queue of block to be flushed.
+    lru_queue: LinkedHashSet<BlockCoordinate>,
+    /// The blocks to be flushed.
+    pending_blocks: HashMap<INum, HashMap<BlockId, (Block, StorageResultSender)>>,
+    /// Receiver to accept commands from `MemoryCache`.
+    command_receiver: mpsc::Receiver<Command>,
+    /// The soft limit of cache.
     limit: SoftLimit,
+    /// The interval of its event loop.
     interval: Duration,
-    storage: Weak<MemoryCache<P, S>>,
-    mut command_receiver: mpsc::Receiver<Command>,
+    /// The limit of command limit.
     command_queue_limit: usize,
-) where
-    P: EvictPolicy<BlockCoordinate> + Send + Sync,
-    S: Storage + Send + Sync,
+}
+
+impl<P, S> WriteBackTask<P, S> {
+    /// Create a write back task.
+    ///
+    /// This task will smoothly write blocks back to the backend
+    /// when the cache runs in write-back policy.
+    ///
+    /// And this task will evict blocks to the backend if the number of blocks
+    /// hits the soft limit, whether the cache runs in write-back policy or
+    /// not.
+    pub(super) fn new(
+        storage: Arc<MemoryCache<P, S>>,
+        command_receiver: mpsc::Receiver<Command>,
+        limit: SoftLimit,
+        interval: Duration,
+        command_queue_limit: usize,
+    ) -> Self {
+        Self {
+            storage,
+            lru_queue: LinkedHashSet::new(),
+            pending_blocks: HashMap::new(),
+            command_receiver,
+            limit,
+            interval,
+            command_queue_limit,
+        }
+    }
+
+    /// Calculate a new period for write back task's interval.
+    fn calculate_period(&self) -> Duration {
+        let pending_length = self.lru_queue.len();
+
+        if pending_length > self.command_queue_limit.overflow_div(4).overflow_mul(3) {
+            // If the number of pending blocks is greater than 75% of `command_queue_limit`,
+            // the new period should be changed to `1/4`.
+            self.interval / 4
+        } else if pending_length > self.command_queue_limit.overflow_div(2) {
+            // If the number of pending blocks is greater than 50% of `command_queue_limit`,
+            // the new period should be changed to `1/2`.
+            self.interval / 2
+        } else {
+            // Otherwise, restore the basic period.
+            self.interval
+        }
+    }
+}
+
+impl<P, S> WriteBackTask<P, S>
+where
+    P: EvictPolicy<BlockCoordinate> + Send + Sync + 'static,
+    S: Storage + Send + Sync + 'static,
 {
-    let period = interval;
-    let mut interval = tokio::time::interval(period);
+    /// Flush a pending block to the backend
+    async fn flush_a_block(&mut self) {
+        if let Some(BlockCoordinate(ino, block_id)) = self.lru_queue.pop_front() {
+            if let Some((block, sender)) = self
+                .pending_blocks
+                .get_mut(&ino)
+                .and_then(|file_level_pending_blocks| file_level_pending_blocks.remove(&block_id))
+            {
+                let res = self.storage.backend().store(ino, block_id, block).await;
+                sender.send(res).unwrap_or_else(|res| {
+                    error!("The receiver is closed unexpectedly, with storage result: {res:?}");
+                });
+            }
+        }
+    }
 
-    let mut lru_queue = LinkedHashSet::new();
-    let mut pending_blocks: HashMap<INum, HashMap<BlockId, (Block, StorageResultSender)>> =
-        HashMap::new();
+    /// Flush a file, write blocks of it to backend immediately.
+    async fn flush_file(&mut self, ino: INum) {
+        let file_level_pending_blocks = self.pending_blocks.remove(&ino);
+        self.lru_queue
+            .retain_with_order(|&BlockCoordinate(i, _)| i != ino);
 
-    loop {
-        // Tune the interval according the payload.
-        let pending_length = lru_queue.len();
-        let new_period = calculate_period(pending_length, command_queue_limit, period);
-        if interval.period() != new_period {
-            interval = tokio::time::interval(new_period);
+        if let Some(file_level_pending_blocks) = file_level_pending_blocks {
+            for (block_id, (block, sender)) in file_level_pending_blocks {
+                let res = self.storage.backend().store(ino, block_id, block).await;
+                sender.send(res).unwrap_or_else(|res| {
+                    error!("The receiver is closed unexpectedly, with storage result: {res:?}");
+                });
+            }
+        }
+    }
+
+    /// Flush all files, write blocks of them to backend immediately.
+    async fn flush_all(&mut self, sender: oneshot::Sender<()>) {
+        self.lru_queue.clear();
+
+        let blocks = mem::take(&mut self.pending_blocks);
+
+        for (ino, file_level_pending_blocks) in blocks {
+            for (block_id, (block, block_sender)) in file_level_pending_blocks {
+                let res = self.storage.backend().store(ino, block_id, block).await;
+                block_sender.send(res).unwrap_or_else(|res| {
+                    error!("The receiver is closed unexpectedly, with storage result: {res:?}");
+                });
+            }
         }
 
-        select! {
-            command = command_receiver.recv() => {
-                match command {
-                    Some(Command::Store { coord: BlockCoordinate(ino, block_id), block, sender }) => {
-                        // Add the block to `lru_queue` and `pending_blocks`
-                        lru_queue.insert(BlockCoordinate(ino, block_id));
-                        pending_blocks.entry(ino).or_default().insert(block_id, (block, sender));
-                    }
-                    Some(Command::Flush(ino)) => {
-                        if let Some(cache) = storage.upgrade() {
-                            flush_file(&mut lru_queue, &mut pending_blocks, cache, ino).await;
-                        } else {
-                            panic!("Memory cache is dropped before being flushed.");
-                        }
-                    },
-                    Some(Command::FlushAll(sender)) => {
-                        if let Some(cache) = storage.upgrade() {
-                            flush_all(&mut lru_queue, &mut pending_blocks, cache, sender).await;
-                        } else {
-                            panic!("Memory cache is dropped before being flushed.");
-                        }
-                    },
-                    Some(Command::Cancel(ino)) => {
-                        pending_blocks.remove(&ino);
-                        lru_queue.retain_with_order(|&BlockCoordinate(i, _)| i != ino);
-                    }
-                    None => {
-                        // The command sender is closed, meaning that the `Storage` is also dropped.
-                        // Then the write back task should exit.
-                        info!("Write back task exits.");
-                        return;
-                    },
+        sender
+            .send(())
+            .unwrap_or_else(|()| warn!("The receiver of pending block is closed unexpectedly."));
+    }
+
+    /// Evict blocks from the cache, if it hits the soft limit.
+    async fn do_evict(&self) {
+        // `a` is a numerator while `b` is a denominator, which are to represent the
+        // soft limit. If `size > capacity * a / b`, the soft limit is considered to
+        // be hit. We use the equivalent inequation `size * b / a > capacity` to
+        // avoid overflowing.
+        let SoftLimit(a, b) = self.limit;
+        let b = b.get();
+
+        let policy = self.storage.policy();
+
+        loop {
+            let capacity = policy.capacity();
+            let size = policy.size();
+
+            // Soft limitation is hit, do evict.
+            if size.overflow_mul(b).overflow_div(a) > capacity {
+                if let Err(e) = self.storage.evict().await {
+                    error!("Failed to evict a block: {e}");
                 }
-            },
-            _ = interval.tick() => {
-                // If the queue is empty, do evict;
-                // Otherwise, flush a block to the backend
-                if let Some(cache) = storage.upgrade() {
-                    if lru_queue.is_empty() {
-                        do_evict(limit, cache).await;
+            } else {
+                break;
+            }
+        }
+    }
+
+    /// Run the task
+    pub(super) async fn run(mut self) {
+        let mut interval = tokio::time::interval(self.interval);
+
+        loop {
+            // Tune the interval according the payload.
+            let new_period = self.calculate_period();
+            if interval.period() != new_period {
+                interval = tokio::time::interval(new_period);
+            }
+
+            select! {
+                command = self.command_receiver.recv() => {
+                    match command {
+                        Some(Command::Store { coord: BlockCoordinate(ino, block_id), block, sender }) => {
+                            // Add the block to `lru_queue` and `pending_blocks`
+                            self.lru_queue.insert(BlockCoordinate(ino, block_id));
+                            self.pending_blocks.entry(ino).or_default().insert(block_id, (block, sender));
+                        }
+                        Some(Command::Flush(ino)) => {
+                            self.flush_file(ino).await;
+                        },
+                        Some(Command::FlushAll(sender)) => {
+                            self.flush_all(sender).await;
+                        },
+                        Some(Command::Cancel(ino)) => {
+                            self.pending_blocks.remove(&ino);
+                            self.lru_queue.retain_with_order(|&BlockCoordinate(i, _)| i != ino);
+                        }
+                        None => {
+                            // The command sender is closed, meaning that the `Storage` is also dropped.
+                            // Then the write back task should exit.
+                            // But it seems to be impossible, because the task holds an `Arc` of cache, which
+                            // holds the sender.
+                            info!("Write back task exits.");
+                            return;
+                        },
+                    }
+                },
+                _ = interval.tick() => {
+                    // If the queue is empty, do evict;
+                    // Otherwise, flush a block to the backend
+                    if self.lru_queue.is_empty() {
+                        self.do_evict().await;
                     } else {
-                        flush_a_block(&mut lru_queue, &mut pending_blocks, cache).await;
+                        self.flush_a_block().await;
                     }
-                } else {
-                    // The command sender is closed, meaning that the `Storage` is also dropped.
-                    // Then the write back task should exit.
-                    info!("Write back task exits.");
-                    return;
-                }
-            },
+                },
+            }
         }
     }
 }
