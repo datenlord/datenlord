7f277373e22cbeeed71f4c78b3f35ff46fa1c1cf
Hifumi Shiroan <shiroan@qq.com>
2024-03-29 04:51:55 +0000

    feat: introduce file handle

---
+++ src/new_storage/handle/handle.rs
@@ -0,0 +1,274 @@
+//! The file handle implementation
+
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+use std::sync::Arc;
+
+use clippy_utilities::Cast;
+use parking_lot::{Mutex, RwLock};
+
+use super::super::backend::Backend;
+use super::super::block::BLOCK_SIZE;
+use super::super::block_slice::offset_to_slice;
+use super::super::error::StorageResult;
+use super::super::policy::LruPolicy;
+use super::super::{CacheKey, MemoryCache};
+use super::reader::Reader;
+use super::writer::Writer;
+
+/// The `FileHandleInner` struct represents the inner state of a file handle.
+/// It contains the file handle, reader, and writer.
+#[derive(Debug)]
+pub struct FileHandleInner {
+    /// The reader.
+    reader: Option<Arc<Reader>>,
+    /// The writer.
+    writer: Option<Arc<Writer>>,
+}
+
+/// The `OpenFlag` enum represents the mode in which a file is opened.
+#[derive(Debug, Clone, Copy)]
+pub enum OpenFlag {
+    /// Open the file for reading.
+    Read,
+    /// Open the file for writing.
+    Write,
+    /// Open the file for reading and writing.
+    ReadAndWrite,
+}
+
+impl FileHandleInner {
+    /// Creates a new `FileHandleInner` instance with the given parameters.
+    #[inline]
+    #[allow(clippy::needless_pass_by_value)]
+    pub fn new(
+        ino: u64,
+        cache: Arc<Mutex<MemoryCache<CacheKey, LruPolicy<CacheKey>>>>,
+        backend: Arc<dyn Backend>,
+        flag: OpenFlag,
+    ) -> Self {
+        let reader = match flag {
+            OpenFlag::Read | OpenFlag::ReadAndWrite => Some(Arc::new(Reader::new(
+                ino,
+                Arc::clone(&cache),
+                Arc::clone(&backend),
+            ))),
+            OpenFlag::Write => None,
+        };
+        let writer = match flag {
+            OpenFlag::Write | OpenFlag::ReadAndWrite => Some(Arc::new(Writer::new(
+                ino,
+                Arc::clone(&cache),
+                Arc::clone(&backend),
+            ))),
+            OpenFlag::Read => None,
+        };
+        FileHandleInner { reader, writer }
+    }
+}
+
+/// The `FileHandle` struct represents a handle to an open file.
+/// It contains an `Arc` of `RwLock<FileHandleInner>`.
+#[derive(Debug, Clone)]
+pub struct FileHandle {
+    /// The file handle.
+    fh: u64,
+    /// The inner file handle
+    inner: Arc<RwLock<FileHandleInner>>,
+}
+
+impl FileHandle {
+    /// Creates a new `FileHandle` instance with the given parameters.
+    pub fn new(
+        fh: u64,
+        ino: u64,
+        cache: Arc<Mutex<MemoryCache<CacheKey, LruPolicy<CacheKey>>>>,
+        backend: Arc<dyn Backend>,
+        flag: OpenFlag,
+    ) -> Self {
+        let inner = FileHandleInner::new(ino, cache, backend, flag);
+        let inner = Arc::new(RwLock::new(inner));
+        FileHandle { fh, inner }
+    }
+
+    /// Returns the file handle.
+    #[must_use]
+    pub fn fh(&self) -> u64 {
+        self.fh
+    }
+
+    /// Gets a reader of this file handle.
+    ///
+    /// # Panic
+    /// Panics if the file handle is not allowed to be read.
+    fn reader(&self) -> Arc<Reader> {
+        self.inner
+            .read()
+            .reader
+            .clone()
+            .unwrap_or_else(|| panic!("This file handle is not allowed to be read."))
+    }
+
+    /// Gets a writer of this file handle.
+    ///
+    /// # Panic
+    /// Panics if the file handle is not allowed to be written.
+    fn writer(&self) -> Arc<Writer> {
+        self.inner
+            .read()
+            .writer
+            .clone()
+            .unwrap_or_else(|| panic!("This file handle is not allowed to be written."))
+    }
+
+    /// Reads data from the file starting at the given offset and up to the
+    /// given length.
+    pub async fn read(&self, offset: u64, len: u64) -> StorageResult<Vec<u8>> {
+        let reader = self.reader();
+        let slices = offset_to_slice(BLOCK_SIZE.cast(), offset, len);
+        let mut buf = Vec::with_capacity(len.cast());
+        reader.read(&mut buf, &slices).await?;
+        Ok(buf)
+    }
+
+    /// Writes data to the file starting at the given offset.
+    pub async fn write(&self, offset: u64, buf: &[u8]) -> StorageResult<()> {
+        let writer = self.writer();
+        let slices = offset_to_slice(BLOCK_SIZE.cast(), offset, buf.len().cast());
+        writer.write(buf, &slices).await
+    }
+
+    /// Extends the file from the old size to the new size.
+    pub async fn extend(&self, old_size: u64, new_size: u64) -> StorageResult<()> {
+        let writer = self.writer();
+        writer.extend(old_size, new_size).await
+    }
+
+    /// Flushes any pending writes to the file.
+    pub async fn flush(&self) {
+        let writer = self.writer();
+        writer.flush().await;
+    }
+
+    /// Closes the writer associated with the file handle.
+    async fn close_writer(&self) {
+        let writer = {
+            let handle = self.inner.read();
+            handle.writer.clone()
+        };
+        if let Some(writer) = writer {
+            writer.close().await;
+        }
+    }
+
+    /// Closes the file handle, closing both the reader and writer.
+    pub async fn close(&self) {
+        self.close_writer().await;
+        if let Some(reader) = self.inner.read().reader.as_ref() {
+            reader.close();
+        }
+    }
+}
+
+/// Number of handle shards.
+const HANDLE_SHARD_NUM: usize = 100;
+
+/// The `Handles` struct represents a collection of file handles.
+/// It uses sharding to avoid lock contention.
+#[derive(Debug)]
+pub struct Handles {
+    /// Use shard to avoid lock contention
+    handles: [Arc<RwLock<Vec<FileHandle>>>; HANDLE_SHARD_NUM],
+}
+
+impl Default for Handles {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Handles {
+    /// Creates a new `Handles` instance.
+    #[must_use]
+    #[inline]
+    pub fn new() -> Self {
+        let mut handles: Vec<_> = Vec::with_capacity(HANDLE_SHARD_NUM);
+        for _ in 0..HANDLE_SHARD_NUM {
+            handles.push(Arc::new(RwLock::new(Vec::new())));
+        }
+        let handles: [_; HANDLE_SHARD_NUM] = handles.try_into().unwrap_or_else(|_| {
+            unreachable!("The length should match.");
+        });
+        Handles { handles }
+    }
+
+    /// Returns the shard index for the given file handle.
+    fn hash(fh: u64) -> usize {
+        let mut hasher = DefaultHasher::new();
+        fh.hash(&mut hasher);
+        (hasher.finish().cast::<usize>()) % HANDLE_SHARD_NUM
+    }
+
+    /// Gets a shard of the fh.
+    fn get_shard(&self, fh: u64) -> &Arc<RwLock<Vec<FileHandle>>> {
+        let idx = Self::hash(fh);
+        self.handles
+            .get(idx)
+            .unwrap_or_else(|| unreachable!("The array is ensured to be long enough."))
+    }
+
+    /// Adds a file handle to the collection.
+    pub fn add_handle(&self, fh: FileHandle) {
+        let shard = self.get_shard(fh.fh());
+        let mut shard_lock = shard.write();
+        shard_lock.push(fh);
+    }
+
+    /// Removes a file handle from the collection.'
+    #[must_use]
+    pub fn remove_handle(&self, fh: u64) -> Option<FileHandle> {
+        let shard = self.get_shard(fh);
+        let mut shard_lock = shard.write();
+        shard_lock
+            .iter()
+            .position(|h| h.fh() == fh)
+            .map(|pos| shard_lock.remove(pos))
+    }
+
+    /// Returns a file handle from the collection.
+    #[must_use]
+    pub fn get_handle(&self, fh: u64) -> Option<FileHandle> {
+        let shard = self.get_shard(fh);
+        let shard_lock = shard.read();
+        let fh = shard_lock.iter().find(|h| h.fh() == fh)?;
+        Some(fh.clone())
+    }
+}
+
+#[cfg(test)]
+#[allow(clippy::unwrap_used)]
+mod tests {
+    use super::*;
+    use crate::new_storage::backend::backend_impl::tmp_fs_backend;
+
+    #[tokio::test]
+    async fn test_file_handle() {
+        let cache = Arc::new(Mutex::new(MemoryCache::new(100)));
+        let backend = Arc::new(tmp_fs_backend().unwrap());
+        let handles = Arc::new(Handles::new());
+        let ino = 1;
+        let fh = 1;
+        let file_handle = FileHandleInner::new(ino, cache, backend, OpenFlag::ReadAndWrite);
+        let file_handle = Arc::new(RwLock::new(file_handle));
+        let file_handle = FileHandle {
+            fh,
+            inner: file_handle,
+        };
+        handles.add_handle(file_handle.clone());
+        let buf = vec![b'1', b'2', b'3', b'4'];
+        file_handle.write(0, &buf).await.unwrap();
+        let read_buf = file_handle.read(0, 4).await.unwrap();
+        assert_eq!(read_buf, buf);
+        file_handle.flush().await;
+    }
+}
---
+++ src/new_storage/handle/writer.rs
@@ -0,0 +1,346 @@
+//! The writer implementation.
+
+use std::sync::Arc;
+
+use bytes::Bytes;
+use clippy_utilities::Cast;
+use parking_lot::{Mutex, RwLock};
+use tokio::sync::mpsc::{Receiver, Sender};
+use tokio::task::JoinHandle;
+use tracing::{error, warn};
+
+use super::super::policy::LruPolicy;
+use super::super::{
+    format_path, offset_to_slice, Backend, Block, BlockSlice, CacheKey, MemoryCache, BLOCK_SIZE,
+};
+use crate::new_storage::{StorageError, StorageResult};
+
+/// The `Writer` struct represents a struct responsible for writing blocks of
+/// data to a backend storage system
+#[derive(Debug)]
+pub struct Writer {
+    /// The inode number associated with the writer
+    ino: u64,
+    /// The cache manager.
+    cache: Arc<Mutex<MemoryCache<CacheKey, LruPolicy<CacheKey>>>>,
+    /// The backend storage system.
+    backend: Arc<dyn Backend>,
+    /// The sender to send tasks to the write back worker.
+    write_back_sender: Sender<Arc<Task>>,
+    /// The handle to the write back worker.
+    write_back_handle: tokio::sync::Mutex<Option<JoinHandle<()>>>,
+    /// The access keys.
+    access_keys: Mutex<Vec<CacheKey>>,
+}
+
+/// The `Task` enum represents the different types of tasks that the write back
+/// worker can perform.
+#[derive(Debug)]
+enum Task {
+    /// A pending write task.
+    Pending(Arc<WriteTask>),
+    /// A flush task.
+    Flush,
+    /// A finish task.
+    Finish,
+}
+
+/// The `WriteTask` struct represents a write task
+#[derive(Debug)]
+struct WriteTask {
+    /// The cache manager.
+    cache: Arc<Mutex<MemoryCache<CacheKey, LruPolicy<CacheKey>>>>,
+    /// The backend storage system.
+    backend: Arc<dyn Backend>,
+    /// The inode number associated with the file being written.
+    ino: u64,
+    /// The block id.
+    block_id: u64,
+    /// The block to be written.
+    block: Arc<RwLock<Block>>,
+}
+
+/// Write a block back to the backend.
+async fn write_back_block(task: Arc<WriteTask>) -> StorageResult<()> {
+    let path = format_path(task.ino, task.block_id);
+    loop {
+        let (content, version) = {
+            let block = task.block.read();
+            if !block.dirty() {
+                // The block has been flushed previously, skip
+                return Ok(());
+            }
+            let content = Bytes::copy_from_slice(block.as_ref());
+            let version = block.version();
+            (content, version)
+        };
+
+        task.backend.write(&path, &content).await?;
+        {
+            let mut block = task.block.write();
+            // Check version
+            if block.version() != version {
+                warn!(
+                    "Version mismatch previous: {}, current: {}",
+                    version,
+                    block.version()
+                );
+                continue;
+            }
+            block.set_dirty(false);
+        }
+        {
+            task.cache.lock().unpin(&CacheKey {
+                ino: task.ino,
+                block_id: task.block_id,
+            });
+            break;
+        }
+    }
+
+    Ok(())
+}
+
+/// Write the blocks to the backend storage system concurrently.
+async fn write_blocks(tasks: &Vec<Arc<WriteTask>>) {
+    let mut handles = Vec::new();
+    for task in tasks {
+        let handle = tokio::spawn(write_back_block(Arc::clone(task)));
+        handles.push(handle);
+    }
+    for handle in handles {
+        match handle.await {
+            Err(e) => {
+                error!("Write back failed: {e}");
+            }
+            Ok(Err(e)) => {
+                error!("Storage error occurs when write back a block: {e}");
+            }
+            _ => {}
+        }
+    }
+}
+
+/// The `write_back_work` function represents the write back worker.
+#[allow(clippy::pattern_type_mismatch)] // Raised by `tokio::select!`
+async fn write_back_work(mut write_back_receiver: Receiver<Arc<Task>>) {
+    //  Create a timer to flush the cache every 200ms.
+    let mut interval = tokio::time::interval(std::time::Duration::from_millis(200));
+    let mut tasks = Vec::new();
+    loop {
+        tokio::select! {
+            Some(task) = write_back_receiver.recv() => {
+                match task.as_ref() {
+                    Task::Pending(task) => {
+                        tasks.push(Arc::clone(task));
+                        if tasks.len() >= 10 {
+                            write_blocks(&tasks).await;
+                            tasks.clear();
+                        }
+                    }
+                    Task::Flush => {
+                        write_blocks(&tasks).await;
+                        tasks.clear();
+                    }
+                    Task::Finish => {
+                        write_blocks(&tasks).await;
+                        tasks.clear();
+                        return;
+                    }
+                }
+            }
+            _ = interval.tick() => {
+                write_blocks(&tasks).await;
+                tasks.clear();
+            }
+        }
+    }
+}
+
+impl Writer {
+    /// Create a new `Writer` with the given inode number, cache manager, and
+    /// backend.
+    #[inline]
+    #[must_use]
+    pub fn new(
+        ino: u64,
+        cache: Arc<Mutex<MemoryCache<CacheKey, LruPolicy<CacheKey>>>>,
+        backend: Arc<dyn Backend>,
+    ) -> Self {
+        let (tx, rx) = tokio::sync::mpsc::channel(100);
+        let mut writer = Writer {
+            ino,
+            cache,
+            backend,
+            write_back_sender: tx,
+            write_back_handle: tokio::sync::Mutex::new(None),
+            access_keys: Mutex::new(Vec::new()),
+        };
+        let handle = tokio::spawn(write_back_work(rx));
+        writer.write_back_handle = tokio::sync::Mutex::new(Some(handle));
+        writer
+    }
+
+    /// Record the block access.
+    fn access(&self, block_id: u64) {
+        let key = CacheKey {
+            ino: self.ino,
+            block_id,
+        };
+        let mut access_keys = self.access_keys.lock();
+        access_keys.push(key);
+    }
+
+    /// Fetch the block from the cache manager.
+    #[inline]
+    pub async fn fetch_block(&self, block_id: u64) -> StorageResult<Arc<RwLock<Block>>> {
+        let key = CacheKey {
+            ino: self.ino,
+            block_id,
+        };
+
+        {
+            let cache = self.cache.lock();
+            if let Some(block) = cache.fetch(&key) {
+                return Ok(block);
+            }
+        }
+
+        let path = format_path(self.ino, block_id);
+        // There is a gap between the block is created and the content is read from the
+        // backend. But according to the current design, concurrency
+        // read/write is not supported.
+        let mut buf = vec![0; BLOCK_SIZE];
+        self.backend.read(&path, &mut buf).await?;
+        let block = {
+            let mut cache = self.cache.lock();
+            cache
+                .new_block(&key, &buf)
+                .ok_or(StorageError::OutOfMemory)?
+        };
+
+        Ok(block)
+    }
+
+    /// Writes data to the file starting at the given offset.
+    #[inline]
+    pub async fn write(&self, buf: &[u8], slices: &[BlockSlice]) -> StorageResult<()> {
+        let mut consume_index = 0;
+        for slice in slices {
+            let block_id = slice.block_id;
+            let end = consume_index + slice.size.cast::<usize>();
+            let write_content = buf
+                .get(consume_index..end)
+                .ok_or(StorageError::OutOfRange {
+                    maximum: buf.len(),
+                    found: end,
+                })?;
+            self.access(block_id);
+            let block = self.fetch_block(block_id).await?;
+            {
+                let mut block = block.write();
+                block.set_dirty(true);
+                let start = slice.offset.cast();
+                let end = start + slice.size.cast::<usize>();
+                let block_size = block.len();
+                block
+                    .get_mut(start..end)
+                    .ok_or(StorageError::OutOfRange {
+                        maximum: block_size,
+                        found: end,
+                    })?
+                    .copy_from_slice(write_content);
+                consume_index += slice.size.cast::<usize>();
+                block.inc_version();
+            }
+            let task = Arc::new(WriteTask {
+                cache: Arc::clone(&self.cache),
+                backend: Arc::clone(&self.backend),
+                ino: self.ino,
+                block_id,
+                block,
+            });
+            self.write_back_sender
+                .send(Arc::new(Task::Pending(task)))
+                .await
+                .unwrap_or_else(|_| {
+                    panic!("Should not send command to write back task when the task quits.");
+                });
+        }
+
+        Ok(())
+    }
+
+    /// Flushes any pending writes to the file.
+    #[inline]
+    pub async fn flush(&self) {
+        self.write_back_sender
+            .send(Arc::new(Task::Flush))
+            .await
+            .unwrap_or_else(|_| {
+                panic!("Should not send command to write back task when the task quits.");
+            });
+    }
+
+    /// Extends the file from the old size to the new size.
+    /// It is only called by the truncate method in the storage system.
+    #[inline]
+    pub async fn extend(&self, old_size: u64, new_size: u64) -> StorageResult<()> {
+        let slices = offset_to_slice(BLOCK_SIZE.cast(), old_size, new_size - old_size);
+        for slice in slices {
+            let buf = vec![0_u8; slice.size.cast()];
+            self.write(&buf, &[slice]).await?;
+        }
+
+        Ok(())
+    }
+
+    /// Closes the writer associated with the file handle.
+    pub async fn close(&self) {
+        self.write_back_sender
+            .send(Arc::new(Task::Finish))
+            .await
+            .unwrap_or_else(|_| {
+                panic!("Should not send command to write back task when the task quits.");
+            });
+        // TODO: handle it by `TaskManager`
+        self.write_back_handle
+            .lock()
+            .await
+            .take()
+            .unwrap_or_else(|| {
+                unreachable!("The `JoinHandle` should not be None.");
+            })
+            .await
+            .unwrap_or_else(|e| {
+                panic!("Failed to join the write back task: {e}");
+            });
+        let keys = self.access_keys.lock();
+        for key in keys.iter() {
+            self.cache.lock().remove(key);
+        }
+    }
+}
+
+#[cfg(test)]
+#[allow(clippy::unwrap_used)]
+mod tests {
+    use super::*;
+    use crate::new_storage::backend::backend_impl::memory_backend;
+
+    const IO_SIZE: usize = 128 * 1024;
+    #[tokio::test]
+    async fn test_writer() {
+        let backend = Arc::new(memory_backend().unwrap());
+        let manger = Arc::new(Mutex::new(MemoryCache::new(10)));
+        let writer = Writer::new(1, Arc::clone(&manger), backend);
+        let content = Bytes::from_static(&[b'1'; IO_SIZE]);
+        let slice = BlockSlice::new(0, 0, content.len().cast());
+        writer.write(&content, &[slice]).await.unwrap();
+        let memory_size = manger.lock().len();
+        assert_eq!(memory_size, 1);
+        writer.close().await;
+        let memory_size = manger.lock().len();
+        assert_eq!(memory_size, 0);
+    }
+}
