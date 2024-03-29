6d413da2979284b0c9b5feb87f829945b853f4ff
Hifumi Shiroan <shiroan@qq.com>
2024-03-29 04:51:55 +0000

    feat: introduce file handle

--- src/new_storage/mod.rs
+++ src/new_storage/mod.rs
@@ -7,6 +7,16 @@
 mod backend;
 mod block;
 mod block_slice;
-pub mod error;
+mod error;
+mod handle;
 mod memory_cache;
 pub mod policy;
+mod utils;
+
+pub use backend::{Backend, BackendImpl};
+pub use block::{Block, BLOCK_SIZE};
+pub use block_slice::{offset_to_slice, BlockSlice};
+pub use error::{StorageError, StorageResult};
+pub use handle::handle::{FileHandle, Handles, OpenFlag};
+pub use memory_cache::{CacheKey, MemoryCache};
+pub use utils::{format_file_path, format_path};
