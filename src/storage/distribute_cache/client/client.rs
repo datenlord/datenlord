d5164589be16fb137e8699ae4db5a6c3e1796b7f
Asklv <boironic@gmail.com>
2024-07-22 14:17:22 +0800

    fix: update shutdown to canceltoken.

--- src/storage/distribute_cache/client/client.rs
+++
@@ -1,9 +0,0 @@
-
-
-pub struct DistributeCacheClient {
-    pub cluster: Arc<Cluster>,
-    pub rpc_client: RpcClient,
-    pub cache: Arc<LocalCache>,
-    pub config: DistributeCacheConfig,
-    pub manager: DistributeCacheManager,
-}
